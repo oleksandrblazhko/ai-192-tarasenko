d28e5b85b27e4aa921abed1864576ff3b084e206
GromZz <81381951+GromZz@users.noreply.github.com>
2023-11-18 15:05:10 +0200

    Create change_data_secure.sql

---
+++ Laboratory-work-8/change_data_secure.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION change_data(attr_1 varchar, attr_2 varchar)
+RETURNS VARCHAR AS $$
+DECLARE 
+	str VARCHAR;
+BEGIN
+	str:= 'UPDATE university SET name = $2 WHERE name = $1';
+	RAISE NOTICE 'Query=%', str;
+	EXECUTE str USING attr_1, attr_2;
+	RETURN 'Name updated';
+END;
+$$ LANGUAGE plpgsql;
