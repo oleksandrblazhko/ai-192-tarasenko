106f1194faea9becd6f1125b8c6035a16745c6c5
GromZz <81381951+GromZz@users.noreply.github.com>
2023-11-18 15:14:21 +0200

    Update change_data_secure.sql

--- Laboratory-work-8/change_data_secure.sql
+++ Laboratory-work-8/change_data_secure.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION change_data(attr_1 varchar, attr_2 varchar)
+CREATE OR REPLACE FUNCTION change_data_secure(attr_1 varchar, attr_2 varchar)
 RETURNS VARCHAR AS $$
 DECLARE 
 	str VARCHAR;
