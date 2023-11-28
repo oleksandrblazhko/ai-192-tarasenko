bf41be969632660f3dd519dead5974f71f0a6755
GromZz <81381951+GromZz@users.noreply.github.com>
2023-11-28 21:32:20 +0200

    Create user_register_secure.sql

---
+++ Laboratory-work-10/user_register_secure.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION user_register_secure(
+    v_user_name varchar, 
+    v_password varchar
+)
+RETURNS INTEGER
+AS $$
+BEGIN
+	IF NOT EXISTS (SELECT FROM user_pass_combo_jay WHERE passname = v_password) THEN
+    IF password_is_correct(v_password) THEN
+      INSERT INTO users (user_name,password_hash) VALUES (v_user_name, md5(v_password)); 
+      RETURN 1;
+    ELSE
+			RAISE NOTICE 'Passname = % incorrect!', v_password;
+			RETURN -1;
+    END IF;
+  ELSE
+    RAISE NOTICE 'Passname = % is bad passname!', v_password;
+    RETURN -1;                                             
+  END IF; 
+END;
+$$ LANGUAGE plpgsql;
